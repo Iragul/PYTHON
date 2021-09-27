f7e619ca2d2d939632e434857362fddea5fd42da
Iragul <75121060+Iragul@users.noreply.github.com>
2021-09-27 20:18:17 +0530

    check odd or even
    I have designed a simple code to check odd or even of n integer using for and if condition

---
+++ odd or even.py
@@ -0,0 +1,9 @@
+print("ENTER NUMBER OF INTEGER WANT TO CHECK")
+i=int(input())
+for k in range(i):
+    print("ENTER THE NUMBER")
+    n=int(input())
+    if n%2 == 0:
+      print(n," IS EVEN NUMBER")
+    else:
+      print(n," IS ODD NUMBER")
