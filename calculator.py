19f9d5d8c3a6c69d47c608d81f2336a0aaddce77
Iragul <75121060+Iragul@users.noreply.github.com>
2021-09-27 19:49:44 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,21 @@
+
+def add(x,y):
+    add=x+y;
+    print(add)
+def sub(x,y):
+    sub=x-y
+    print(sub)
+def mul(x,y):
+    mul=x*y
+    print(mul)
+def div(x,y):
+    div=x/y
+    print(div)
+a=int(input())
+b=int(input())
+add(a,b)
+sub(a,b)
+mul(a,b)
+div(a,b)
+
+    
