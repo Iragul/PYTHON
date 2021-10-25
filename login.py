fe1dbe7977c18284282b822a2393e193968b9e7f
Iragul <75121060+Iragul@users.noreply.github.com>
2021-10-25 19:40:57 +0530

    a piece of code to login

---
+++ login.py
@@ -0,0 +1,6 @@
+user=input("ENTER THE USER NAME \n")
+password=input("ENTER THE PASSWORD \n")
+if(user=="admin" and password=="1234"):
+        print("login sucess")
+else:
+     print("password wrong")
